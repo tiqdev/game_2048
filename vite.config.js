1468642f5d94cfe7a816d39727dbd03ebcbee562
Tarık KAYA <tiqqq24@gmail.com>
2023-10-31 21:54:01 +0300

    pwa setup testing 1

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from "path"
+import { VitePWA } from "vite-plugin-pwa";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    manifest: {
+      name: 'My Awesome App',
+      short_name: 'MyApp',
+      description: 'My Awesome App description',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+    workbox: {
+      clientsClaim: true,
+      skipWaiting: true
+    }
+  })],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'src'),
     },
   },
 })
+
