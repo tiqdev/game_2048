7637a68870822d6d04e1deb45947650622905574
Tarık KAYA <tiqqq24@gmail.com>
2023-11-02 13:56:14 +0300

    metadata modified

--- vite.config.js
+++ vite.config.js
@@ -20,12 +20,12 @@ export default defineConfig({
       start_url: '/',
       icons: [
         {
-          src: '192.png',
+          src: 'image/png/192.png',
           sizes: '192x192',
           type: 'image/png'
         },
         {
-          src: '512.png',
+          src: 'image/png/512.png',
           sizes: '512x512',
           type: 'image/png'
         }
