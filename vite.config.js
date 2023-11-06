4687a62535f205c2ddc320a1118aaea29f3260c1
Tarık KAYA <tiqqq24@gmail.com>
2023-11-06 19:20:00 +0300

    maskable icon added

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
-    includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', "mask-icon.png"],
     registerType: 'autoUpdate',
     injectRegister: 'auto',
 
