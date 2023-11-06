8eb16a84bd0129a7ace247b6c925e79f80ba5504
Tarık KAYA <tiqqq24@gmail.com>
2023-11-06 19:23:23 +0300

    manifest modified

--- vite.config.js
+++ vite.config.js
@@ -30,6 +30,12 @@ export default defineConfig({
           "src": "/image/png/512.png",
           "sizes": "512x512",
           "type": "image/png"
+        },
+        {
+          "src": "/image/png/mask-icon.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "any maskable"
         }
       ]
     },
